2870e250e1b6b55df4272ee7b09ac759fddac2ce
Pepialarcon <mdafrica24@gmail.com>
2025-11-07 18:40:38 +0100

    inclusion datos imagen

--- src/pages/Exposicion.jsx
+++ src/pages/Exposicion.jsx
@@ -1,9 +1,18 @@
 import Picture from "../components/Picture"
+
 function Exposicion() {
+    const pictureExample = {
+        id: 1,
+        name: "¨La noche estrellada",
+        image: "",
+        description: "Una noche genial y hermosa"
+    }
+
     return (
         <div>
             <h1>exposicion</h1>
-            <Picture/>
+            <Picture pictureProp={pictureExample} />
+
         </div>
     )
 }
