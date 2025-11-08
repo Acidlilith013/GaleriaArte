05681de987d035e02ad331da0cd9e43b546a6786
Javier Vera <curso@zaramail.com>
2025-11-08 13:13:15 +0100

    folder create pictures and Gallery

--- src/data/dataGallery.js
+++ src/data/dataGallery.js
@@ -3,31 +3,31 @@ const dataGallery = [
     {
         id: 1,
         name: "Sala principal",
-        image: "src/img/gallery-1.jpg",
+        image: "src/img/JV/gallery-1.jpg",
         description: "Sala Principal dotada de 140m"
     },
     {
         id: 2,
         name: "Sala exposición 2",
-        image: "src/img/gallery-2.jpg",
+        image: "src/img/JV/gallery-12.png",
         description: "Espacio secundario con luz natural."
     },
     {
         id: 3,
         name: "Sala escultura",
-        image: "src/img/gallery-4.jpg",
+        image: "src/img/JV/gallery-4.jpg",
         description: "Área dedicada a obras tridimensionales."
     },
     {
         id: 4,
         name: "Sala temporal",
-        image: "src/img/gallery-3.jpg",
+        image: "src/img/JV/gallery-3.jpg",
         description: "Exhibiciones rotativas de artistas invitados."
     },
     {
         id: 5,
         name: "Sala multimedia",
-        image: "src/img/gallery-5.jpg",
+        image: "src/img/JV/gallery-5.jpg",
         description: "Instalaciones con video y sonido."
     }
 ];
