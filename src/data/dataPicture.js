7f33c2e29dbd85a9f96998b7f97c95bb772efdc0
Pepialarcon <mdafrica24@gmail.com>
2025-11-11 12:14:38 +0100

    modificaciones imagenes exposicion

--- src/data/dataPicture.js
+++ src/data/dataPicture.js
@@ -13,7 +13,7 @@ const dataPicture=[
     name: "Paisaje invernal con iglesia",
     autor: "Barend Cornelis Koekkoek",
     año: 1850,
-    image: "src\img\Pepi\Image20251108130951.png",
+    image: "src/img/Pepi/Image20251108130951.png",
     description: "Una aldea bajo la nieve, con una iglesia destacando en el centro."
   },
   {
@@ -21,7 +21,7 @@ const dataPicture=[
     name: "Molino de viento a orillas de un canal",
     autor: "George Henry Boughton",
     año: 1888,
-    image: "http://googleusercontent.com/image_collection/image_retrieval/2305800785416755423_2",
+    image: "src/img/Pepi/Image20251108130958.png",
     description: "Molino de viento cerca del agua con una atmósfera tranquila y nublada."
   },
   {
@@ -29,15 +29,15 @@ const dataPicture=[
     name: "Estudio de las olas del mar",
     autor: "Gustave Courbet",
     año: 1869,
-    image: "http://googleusercontent.com/image_collection/image_retrieval/2305800785416755423_3",
+    image: "src/img/Pepi/Image20251108131014.jpg",
     description: "Representación dramática y detallada de la espuma de las olas rompiendo."
   },
-  {
+  { 
     id: 5,
     name: "Un sendero en el bosque",
     autor: "Julian Rix",
     año: 1890,
-    image: "http://googleusercontent.com/image_collection/image_retrieval/2305800785416755423_4",
+    image: "src/img/Pepi/Image20251108131153.png",
     description: "Un camino serpenteante bajo la densa vegetación de un bosque, con luz filtrada."
   }
 ]
--- src/pages/Exposiciones.jsx
+++ src/pages/Exposiciones.jsx
@@ -2,18 +2,20 @@ import Picture from "../components/Picture"
 import dataPicture from "../data/dataPicture";
 import { useState } from "react";
 
-function Exposicion () {
-        const [exposicionState, setExposicionState] = useState (dataPicture);
-        
-        return (
-            <>
-    <div>
-        <h1>Galería</h1>
-        {dataPicture.map((dataPictureSingle) => {
-            return <Picture pictureProp={dataPictureSingle} />
-        })}
-    </div>
-    </>
-)
+function Exposicion() {
+    const [exposicionState, setExposicionState] = useState(dataPicture);
+
+    return (
+        <>
+            <div>
+                <h1>Galería</h1>
+                {exposicionState.map((dataPictureSingle) => {
+                    return <Picture
+                        key={dataPictureSingle.id}
+                        pictureProp={dataPictureSingle} />
+                })}
+            </div>
+        </>
+    )
 }
 export default Exposicion
