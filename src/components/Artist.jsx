e186741f1d932951ff0285041ae78c0f1051cfcc
Javier Vera <curso@zaramail.com>
2025-11-15 12:33:27 +0100

    cambios para que funcione la web

--- src/components/Artist.jsx
+++ src/components/Artist.jsx
@@ -1,14 +1,6 @@
 function Artist({ artistProp, onDelete }) {
     const { id, name, description, age, image } = artistProp;
     return (
-<<<<<<< HEAD
-        <div className="artist-target">
-            <p>{id}</p>
-            <p>{name}</p>
-            <p className="description-artist">{description}</p>
-            <p>{age}</p>
-            <img className="picture-artist" src={image} alt="Artistas" />
-=======
         <div className="artist-card">
             <div className="artist-image">
                 <img src={image} alt="Artistas" />
@@ -21,7 +13,6 @@ function Artist({ artistProp, onDelete }) {
             </div>
             <button onClick={() => onDelete(id)}>Eliminar</button>
 
->>>>>>> 792565864d951ad94a678b19adf02bcb9f051596
 
         </div>
 
