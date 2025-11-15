3d52de3dee523185dc8bf4cea6b77f882b652c8b
Acidlilith013 <nuriagaray632@gmail.com>
2025-11-15 11:10:14 +0100

    añado boton y lo elimino

--- src/components/Artist.jsx
+++ src/components/Artist.jsx
@@ -1,14 +1,21 @@
-function Artist({ artistProp }) {
+function Artist({ artistProp, onDelete }) {
     const { id, name, description, age, image } = artistProp;
     return (
-        <div>
-            <p>{id}</p>
-            <p>{name}</p>
-            <p>{description}</p>
-            <p>{age}</p>
-            <img src={image} alt="Artistas" />
+        <div className="artist-card">
+            <div className="artist-image">
+                <img src={image} alt="Artistas" />
+            </div>
+            <div className="artist-body">
+                <p>{id}</p>
+                <p>{name}</p>
+                <p>{description}</p>
+                <p>{age}</p>
+            </div>
+            <button onClick={() => onDelete(id)}>Eliminar</button>
+
 
         </div>
+
 
     )
 
